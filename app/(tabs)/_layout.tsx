f4b67453620d1dcec934efc866edb650ba68dcd3
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-16 11:30:40 +0100

    enhance tab navigation with screen options and icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,32 @@
 import { Tabs } from 'expo-router';
+import IonicIcons from '@expo/vector-icons/Ionicons';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#ffffff',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <IonicIcons
+              name={focused ? 'home-sharp' : 'home-outline'}
+              size={24}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -19,6 +39,17 @@ export default function TabsLayout() {
         name="about"
         options={{
           title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <IonicIcons
+              name={
+                focused
+                  ? 'information-circle-sharp'
+                  : 'information-circle-outline'
+              }
+              size={24}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
