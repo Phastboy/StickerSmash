84ce3ed072f451be87abdf8955d75527c1397f5d
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-16 11:15:06 +0100

    bottom tab navigator

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,26 @@
+import { Tabs } from 'expo-router';
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+        }}
+      />
+    </Tabs>
+  );
+}
