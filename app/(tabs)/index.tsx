911202e75bbd3866dd930ebd6a40f7a9905abc22
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-04-16 17:38:50 +0100

    add Button component and integrate with Index; update package dependencies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
+import { View, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-import { StyleSheet, View } from 'react-native';
 
-const PlaceHolderImage = require('../../assets/images/background-image.png');
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceHolderImage} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -17,9 +23,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
